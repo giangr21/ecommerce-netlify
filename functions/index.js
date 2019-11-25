0fb028745d4087b871e80edd87fb7f9351167568
Gian <giangr21@gmail.com>
2019-11-24 22:21:03 -0300

    Ajuste

--- functions/index.js
+++ functions/index.js
@@ -8,16 +8,6 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY),
 
 exports.handler = async (event, context) => {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hi there Tacos",
-      event
-    })
-  }
-
-
-
 
   if (!event.body || event.httpMethod !== "POST") {
     return {
